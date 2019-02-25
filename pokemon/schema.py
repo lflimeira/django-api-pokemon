30246739cf8f1227cb54bc64a3b944256cc9b7c1
Luiz Felipe <lflimeira02@gmail.com>
2019-02-25 00:41:31 -0300

    users: create user schema and JWT authentication

--- pokemon/schema.py
+++ pokemon/schema.py
@@ -1,11 +1,15 @@
 import graphene
+import graphql_jwt
 
 import trainers.schema
+import users.schema
 
-class Query(trainers.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, trainers.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(trainers.schema.Mutation, graphene.ObjectType):
-    pass
+class Mutation(users.schema.Mutation, trainers.schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
